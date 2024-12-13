eed42574e9498be5a095a0ac732e0907af87e0dc
NhaPhuowng <phuongptn03@gmail.com>
2024-12-13 20:47:08 +0700

    commit

---
+++ QA_demoMIS.py
@@ -0,0 +1,290 @@
+import dimod
+import networkx as nx
+import time
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import dimod
+import pandas as pd
+from ortools.linear_solver import pywraplp
+
+# token = "DEV-7affe1a83dbe06fa17c9a260577608396c251455"
+# token = "DEV-b28f5c26b9419829978caa8899867ab5c25f9802"
+
+# DEV-898779584c4bed23fcf5bcbd657344d29493c2b9
+
+#DEV-1b5e467ff8bc1e44f94062b62fc90af26dbe982e
+#pip install dwave-ocean-sdk
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+from dwave.samplers import SimulatedAnnealingSampler
+from itertools import combinations
+
+
+def create_mis_qubo(graph, penalty_weight=1.0):
+    """
+    Create a QUBO formulation for the Maximum Independent Set problem.
+
+    Parameters:
+        graph (networkx.Graph): Input graph with nodes and edges.
+        penalty_weight (float): Weight for the penalty term.
+
+    Returns:
+        dimod.BinaryQuadraticModel: The QUBO for the Maximum Independent Set.
+    """
+    bqm = dimod.BinaryQuadraticModel('BINARY')
+
+    # Add linear terms for maximizing the size of the independent set
+    for node in graph.nodes:
+        bqm.add_variable(node, -1)  # Coefficient for H1: -x_i
+
+    # Add quadratic terms for penalizing adjacent nodes in the set
+    for edge in graph.edges:
+        i, j = edge
+        bqm.add_interaction(i, j, penalty_weight)  # Coefficient for H2: lambda * x_i * x_j
+
+    return bqm
+
+def count_num_penalty(response_data, graph):
+    total_penalty = 0
+    for data in response_data:
+        res = data.sample
+        test_list = []  # khoi tao danh sach cac dinh duoc chon
+        penalty = 0  # khoi tao so lan vi pham rang buoc
+
+        # tao list cac dinh duoc chon
+        for i in res.keys():
+            #print(res.get(i), end=" ")
+            if res.get(i) == 1:
+                 test_list.append(i)
+
+        # tu list do tao nen cac cap canh kha thi, dem so cap thuoc do thi G
+        possible_edge = list(combinations(test_list, 2))
+        for x in possible_edge:
+            if (x in G.edges):
+                penalty += 1
+                total_penalty += 1
+    return total_penalty
+
+def count_percet_solution(response_data, lowest_energy):
+    num_of_correct_solution = 0
+    for data in response_data:
+        if (data.energy == lowest_energy_orTools):
+            num_of_correct_solution += data.num_occurrences
+    return num_of_correct_solution
+
+def count_denity_graph(n, num_edges):
+    return 2 * num_edges / (n * (n - 1))
+
+def create_random_graph(n, num_edges):
+    # Tạo danh sách các cạnh trong đồ thị đầy đủ
+    edges = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            edges.append((i, j))
+
+    # Chọn ngẫu nhiên 147 cạnh từ danh sách
+    random.shuffle(edges)
+    selected_edges = edges[:num_edges]
+    
+    input_folder = "input_data"  # Thư mục chứa các file TXT
+    file_to_read = "data_15.txt"  # File cần đọc
+
+    # Đường dẫn đầy đủ đến file
+    file_path = os.path.join(input_folder, file_to_read)
+    # In các cạnh theo yêu cầu
+    if os.path.exists(file_path):
+        with open(file_path, "w") as f:
+            for edge in selected_edges:
+                f.write(f"{edge[0]} {edge[1]}\n")
+
+    print(f"Đồ thị với {n} đỉnh, {num_edges} cạnh, có mật độ {count_denity_graph(n, num_edges)} đã được lưu vào file {file_path}")
+
+    
+def maximum_weighted_independent_set(weights, edges):
+    # Create the solver
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+    if not solver:
+        return None
+
+    num_nodes = len(weights)
+
+    # Decision variables: x[i] = 1 if node i is in the independent set, 0 otherwise
+    x = {}
+    for i in range(num_nodes):
+        x[i] = solver.BoolVar(f'x[{i}]')
+
+    # Objective function: maximize sum of weights * x[i]
+    objective = solver.Objective()
+    for i in range(num_nodes):
+        objective.SetCoefficient(x[i], weights[i])
+    objective.SetMaximization()
+
+    # Constraints: For each edge (i, j), ensure that x[i] + x[j] <= 1
+    for (i, j) in edges:
+        solver.Add(x[i] + x[j] <= 1)
+
+    # Solve the problem
+    status = solver.Solve()
+    print('TimeRunning %f' % (solver.wall_time() / 1000.0))
+    print('Problem solved in %d iterations' % solver.iterations())
+    # Check if a solution was found
+    if status == pywraplp.Solver.OPTIMAL:
+        print('OptimalSolution', solver.Objective().Value())
+        independent_set = [i for i in range(num_nodes) if x[i].solution_value() == 1]
+        total_weight = sum(weights[i] for i in independent_set)
+        return total_weight
+    else:
+        return 0
+
+data_for_df = []
+if __name__ == "__main__":
+    
+    # create radom graph and save to folder input
+    # random.seed(15)
+    # create_random_graph(40, 312)
+    
+    
+    input_folder = "input_data"
+    output_folder = "NEW/PickAnT/gamma2_AnT200"
+    output_csv = "NEW/output_csv"
+    
+    
+    for i in range(1, 16):
+        file_to_read = "data_" + str(i) + ".txt"
+        file_path = os.path.join(input_folder, file_to_read)
+        print(file_path)
+        
+        if os.path.exists(file_path):
+            print(file_path)
+            G = nx.Graph()
+            with open(file_path, "r") as file:
+                for line in file:
+                    u, v = map(int, line.split()) 
+                    G.add_edge(u, v)
+                #print(G.edges)
+        
+        list_of_ones = [1] * len(G.nodes)
+        res_ortools = maximum_weighted_independent_set(list_of_ones, G.edges())
+    
+        penalty_weigth_num = 2
+        Q = create_mis_qubo(G, penalty_weight=penalty_weigth_num)
+        #print(Q)
+    
+        chainstrength = 8
+        numruns = 1000
+        annealingTime = 200
+        sampler = EmbeddingComposite(DWaveSampler(token='DEV-1b5e467ff8bc1e44f94062b62fc90af26dbe982e'))
+        response = sampler.sample(Q,
+                               chain_strength=chainstrength,
+                               num_reads=numruns,
+                               annealing_time=annealingTime,
+                               label='Maximum Independent Set')
+    
+
+        response = sampler.sample(Q, num_reads = 1000)
+        
+        timing_info = response.info["timing"]
+        
+    
+
+        lowest_energy = response.first.energy
+        lowest_energy_orTools = - res_ortools
+    
+        for data in response.data():
+            print(data)
+        print("-------------------------------------------")
+        print("Gamma:", penalty_weigth_num)
+        print("Annealing_time:", annealingTime)
+        print("So dinh:", G.number_of_nodes())
+        print("So canh:", G.number_of_edges())
+        print("Mat do do thi:", count_denity_graph(G.number_of_nodes(), G.number_of_edges()))
+        print("Nang luong thap nhat bi sai ban dau la: ", lowest_energy)
+        print("So lan vi pham rang buoc: ", count_num_penalty(response.data(), G))
+        print("So solution dung la:", count_percet_solution(response.data(), lowest_energy_orTools))
+        print("Phan tram so cau tra loi dung: ", count_percet_solution(response.data(), lowest_energy_orTools)/10)
+        print("Best solutions are {}% of samples.".format(len(response.lowest(atol=0.5).record.energy)/10))
+        print(response.info["timing"])
+        print("Nang luong thap nhat va loi giai toi uu ortools: ", res_ortools)
+        print("qpu_anneal_time_per_sample:", timing_info['qpu_anneal_time_per_sample'])
+        print("qpu_access_time:", timing_info['qpu_access_time'])
+        
+
+
+        # Save data to folder out_put
+        file_to_write = "QA_gamma2_anT200" + str(i) +".json"
+        file_path_write = os.path.join(output_folder, file_to_write)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        output_data = []
+
+        for data in response.data():
+            energy = data.energy
+            num_occurrences = data.num_occurrences
+            chain_break_fraction = data.chain_break_fraction
+            sample_info = {
+            "energy": energy,
+            "num_occurrences": int(num_occurrences),
+            "chain_break_fraction": chain_break_fraction
+            }
+            output_data.append(sample_info)
+    
+        result_info = {
+            "Gamma": penalty_weigth_num,
+            "Annealing_time": annealingTime,
+            "So dinh": G.number_of_nodes(),
+            "So canh": G.number_of_edges(),
+            "Mat do do thi": count_denity_graph(G.number_of_nodes(), G.number_of_edges()),
+            "Nang luong thap nhat bi sai ban dau": lowest_energy,
+            "vi pham": count_num_penalty(response.data(), G),
+            "solution dung": int(count_percet_solution(response.data(), lowest_energy_orTools)),
+            "Percent solution dung": count_percet_solution(response.data(), lowest_energy_orTools)/1000,
+            "Best solutions of samples %": format(len(response.lowest(atol=0.5).record.energy)/10),
+            "Thoi gian chay": response.info["timing"],
+            "MIS Ortools": res_ortools
+        }
+        output_data.append(result_info)
+
+        try:
+            with open(file_path_write, "w") as output_file:
+                json.dump(output_data, output_file, indent=4)
+            print(f"Results have been saved to {file_path_write}")
+        except Exception as e:
+            print(f"Error while writing JSON: {e}")
+        
+        
+        #to Data Frame
+        data_for_df.append({ # type: ignore
+                "file_read": file_to_read,
+                "file_write": file_to_write,
+                "num_nodes": G.number_of_nodes(),
+                "num_edges": G.number_of_edges(),
+                "graph_density": 2 * len(G.edges) / (G.number_of_nodes() * (G.number_of_nodes() - 1)),
+                "gamma": penalty_weigth_num,
+                "annealing_time": annealingTime,
+                "lowest_energy_dwave": lowest_energy,
+                "lowest_energy_or_tools": lowest_energy_orTools,
+                "vi pham": count_num_penalty(response.data(), G),
+                "correct_solutions": count_percet_solution(response.data(), lowest_energy_orTools),
+                "percentage_correct": count_percet_solution(response.data(), lowest_energy_orTools) / 10,
+                "qpu_anneal_time_per_sample": timing_info['qpu_anneal_time_per_sample'],
+                "qpu_access_time": timing_info['qpu_access_time']
+                
+            })
+        
+    df = pd.DataFrame(data_for_df) # type: ignore
+    print(df)
+    
+    if not os.path.exists(output_csv):
+            os.makedirs(output_csv)
+            
+    df.to_csv(os.path.join(output_csv, "QA_gamma2_anT200.csv"), index=False)
+
+    print("Data has been saved to gamma0_5_AnTime50_again.csv")
+
+                
+
+   
---
+++ demo.py
@@ -0,0 +1,243 @@
+import dimod
+import networkx as nx
+import time
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import dimod
+from ortools.linear_solver import pywraplp
+
+# token = "DEV-7affe1a83dbe06fa17c9a260577608396c251455"
+# token = "DEV-b28f5c26b9419829978caa8899867ab5c25f9802"
+
+# DEV-898779584c4bed23fcf5bcbd657344d29493c2b9
+#pip install dwave-ocean-sdk
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+from dwave.samplers import SimulatedAnnealingSampler
+from itertools import combinations
+
+def create_mis_qubo(graph, penalty_weight=1.0):
+    """
+    Create a QUBO formulation for the Maximum Independent Set problem.
+
+    Parameters:
+        graph (networkx.Graph): Input graph with nodes and edges.
+        penalty_weight (float): Weight for the penalty term.
+
+    Returns:
+        dimod.BinaryQuadraticModel: The QUBO for the Maximum Independent Set.
+    """
+    bqm = dimod.BinaryQuadraticModel('BINARY')
+
+    # Add linear terms for maximizing the size of the independent set
+    for node in graph.nodes:
+        bqm.add_variable(node, -1)  # Coefficient for H1: -x_i
+
+    # Add quadratic terms for penalizing adjacent nodes in the set
+    for edge in graph.edges:
+        i, j = edge
+        bqm.add_interaction(i, j, penalty_weight)  # Coefficient for H2: lambda * x_i * x_j
+
+    return bqm
+
+def count_num_penalty(response_data, graph):
+    total_penalty = 0
+    for data in response_data:
+        res = data.sample
+        test_list = []  # khoi tao danh sach cac dinh duoc chon
+        penalty = 0  # khoi tao so lan vi pham rang buoc
+
+        # tao list cac dinh duoc chon
+        for i in res.keys():
+            #print(res.get(i), end=" ")
+            if res.get(i) == 1:
+                 test_list.append(i)
+
+        # tu list do tao nen cac cap canh kha thi, dem so cap thuoc do thi G
+        possible_edge = list(combinations(test_list, 2))
+        for x in possible_edge:
+            if (x in G.edges):
+                penalty += 1
+                total_penalty += 1
+    return total_penalty
+
+def count_percet_solution(response_data, lowest_energy):
+    num_of_correct_solution = 0
+    for data in response_data:
+        if (data.energy == lowest_energy):
+            num_of_correct_solution += data.num_occurrences
+    return num_of_correct_solution
+
+def count_denity_graph(n, num_edges):
+    return 2 * num_edges / (n * (n - 1))
+
+def create_random_graph(n, num_edges):
+    # Tạo danh sách các cạnh trong đồ thị đầy đủ
+    edges = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            edges.append((i, j))
+
+    # Chọn ngẫu nhiên 147 cạnh từ danh sách
+    random.shuffle(edges)
+    selected_edges = edges[:num_edges]
+    
+    input_folder = "input_data"  # Thư mục chứa các file TXT
+    file_to_read = "data_1.txt"  # File cần đọc
+
+    # Đường dẫn đầy đủ đến file
+    file_path = os.path.join(input_folder, file_to_read)
+    # In các cạnh theo yêu cầu
+    if os.path.exists(file_path):
+        with open(file_path, "w") as f:
+            for edge in selected_edges:
+                f.write(f"{edge[0]} {edge[1]}\n")
+
+    print(f"Đồ thị với {n} đỉnh, {num_edges} cạnh, có mật độ {count_denity_graph(n, num_edges)} đã được lưu vào file {file_path}")
+
+    
+def maximum_weighted_independent_set(weights, edges):
+    # Create the solver
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+    if not solver:
+        return None
+
+    num_nodes = len(weights)
+
+    # Decision variables: x[i] = 1 if node i is in the independent set, 0 otherwise
+    x = {}
+    for i in range(num_nodes):
+        x[i] = solver.BoolVar(f'x[{i}]')
+
+    # Objective function: maximize sum of weights * x[i]
+    objective = solver.Objective()
+    for i in range(num_nodes):
+        objective.SetCoefficient(x[i], weights[i])
+    objective.SetMaximization()
+
+    # Constraints: For each edge (i, j), ensure that x[i] + x[j] <= 1
+    for (i, j) in edges:
+        solver.Add(x[i] + x[j] <= 1)
+
+    # Solve the problem
+    status = solver.Solve()
+    print('TimeRunning %f' % (solver.wall_time() / 1000.0))
+    print('Problem solved in %d iterations' % solver.iterations())
+    # Check if a solution was found
+    if status == pywraplp.Solver.OPTIMAL:
+        print('OptimalSolution', solver.Objective().Value())
+        independent_set = [i for i in range(num_nodes) if x[i].solution_value() == 1]
+        total_weight = sum(weights[i] for i in independent_set)
+        return total_weight
+    else:
+        return 0
+
+if __name__ == "__main__":
+    # create radom graph and save to folder input
+    # random.seed(1)
+    # create_random_graph(40, 312)
+    input_folder = "input_data"  # Thư mục chứa các file TXT
+    file_to_read = "data_15.txt"  # File cần đọc
+
+    # Đường dẫn đầy đủ đến file
+    file_path = os.path.join(input_folder, file_to_read)
+
+    # Kiểm tra nếu file tồn tại
+    if os.path.exists(file_path):
+        # Tạo đồ thị từ file
+        G = nx.Graph()
+        with open(file_path, "r") as file:
+            for line in file:
+                u, v = map(int, line.split())  # Đọc các cạnh từ file
+                G.add_edge(u, v)
+    # print(G.edges)
+                
+    # list_of_ones = [1] * len(G.nodes)
+    # res_ortools = maximum_weighted_independent_set(list_of_ones, G.edges())
+    #print("Ortool: ", res_ortools)
+    
+    # Generate QUBO
+    penalty_weigth_num = 2.0
+    Q = create_mis_qubo(G, penalty_weight=penalty_weigth_num)
+ 
+    sampler = SimulatedAnnealingSampler()
+
+    response = sampler.sample(Q, num_reads = 1000)
+    
+    # using ortool
+    list_of_ones = [1] * len(G.nodes)
+    res_ortools = maximum_weighted_independent_set(list_of_ones, G.edges())
+    lowest_energy_orTools = - res_ortools
+    #print("Ortool: ", res_ortools)
+    
+    # print(response['time'])
+    print(response.info)
+    print("------------")
+    print(response)
+    print("------------")
+    #rint(response[timing])
+    
+    for data in response.data():
+        print(data)
+    print("-------------------------------------------")
+  
+    print("So dinh:", G.number_of_nodes())
+    print("So canh:", G.number_of_edges())
+    print("Mat do do thi:", count_denity_graph(G.number_of_nodes(), G.number_of_edges()))
+    print("Nang luong thap nhat bi sai ban dau la: ", response.first.energy)
+    print("So lan vi pham rang buoc: ", count_num_penalty(response.data(), G))
+    print("So solution dung la:", count_percet_solution(response.data(), lowest_energy_orTools))
+    print("Phan tram so cau tra loi dung: ", count_percet_solution(response.data(), lowest_energy_orTools)/10)
+    print("Best solutions are {}% of samples.".format(len(response.lowest(atol=0.5).record.energy)/10))
+    print("penalty_weigth_num, penalty_weigth_num")
+    print(response.info["timing"])
+    #print("Nang luong thap nhat theo Exact Solver: ", min_energy_ExactSolver)
+    print("Nang luong thap nhat va loi giai toi uu ortools: ", res_ortools)
+    
+    output_folder = "SA_new_gamma2"
+    file_to_write = "SA_15.json"
+    file_path_write = os.path.join(output_folder, file_to_write)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Chuẩn bị dữ liệu để ghi vào file JSON
+    output_data = []
+
+    # Chuyển đổi dữ liệu từ Sample thành dạng có thể lưu vào JSON
+    for data in response.data():
+        #sample = data.sample
+        energy = data.energy
+        num_occurrences = data.num_occurrences
+        sample_info = {
+        #"sample": data.sample,  # Đưa dictionary sample vào (các giá trị như {0: 0, 1: 1, ...})
+        "energy": energy,
+        "num_occurrences": int(num_occurrences)
+        }
+        output_data.append(sample_info)
+    
+    result_info = {
+        "So dinh": G.number_of_nodes(),
+        "So canh": G.number_of_edges(),
+        "Mat do do thi": count_denity_graph(G.number_of_nodes(), G.number_of_edges()),
+        "Nang luong thap nhat bi sai ban dau": response.first.energy,
+        "So lan vi pham rang buoc": count_num_penalty(response.data(), G),
+        "So solution dung la": int(count_percet_solution(response.data(), lowest_energy_orTools)),
+        "Phan tram so cau tra loi dung": count_percet_solution(response.data(), lowest_energy_orTools)/1000,
+        "Best solutions of samples %": format(len(response.lowest(atol=0.5).record.energy)/10),
+        "penalty_weigth_num": penalty_weigth_num,
+        "Thoi gian chay": response.info["timing"],
+        #"Nang luong thap nhat theo ExactSolver": min_energy_ExactSolver,
+        "Nang luong thap nhat va cac loi giai toi uu theo ortools": res_ortools
+    }
+    output_data.append(result_info)
+
+    # Lưu dữ liệu vào file JSON
+    try:
+        with open(file_path_write, "w") as output_file:
+            json.dump(output_data, output_file, indent=4)
+        print(f"Results have been saved to {file_path_write}")
+    except Exception as e:
+        print(f"Error while writing JSON: {e}")
